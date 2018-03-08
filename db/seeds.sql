8be12a466cd2a8b427267405ce14a278e8b346f0
wzuschlag <wzuschlag@gmail.com>
2018-03-08 17:06:49 -0600

    update all files

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Deluxe', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Texas', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('BLT', false);
